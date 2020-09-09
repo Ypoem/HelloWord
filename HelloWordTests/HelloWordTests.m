8c8e45b927ce0fd5248dfd4d417a4d5292dc0f62
张诗雨 <zhangshiyu@bicai365.com>
2020-09-09 16:06:30 +0800

    code

---
+++ HelloWordTests/HelloWordTests.m
@@ -0,0 +1,37 @@
+//
+//  HelloWordTests.m
+//  HelloWordTests
+//
+//  Created by 张诗雨 on 2020/9/9.
+//  Copyright © 2020 openapi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HelloWordTests : XCTestCase
+
+@end
+
+@implementation HelloWordTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'HelloWord' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for HelloWord
+
+  target 'HelloWordTests' do
+    inherit! :search_paths
+    # Pods for testing
+    pod 'AFNetworking', '3.2.1'
+    pod 'MBProgressHUD', '~> 1.2.0'
+    pod 'Masonry', '~>1.1.0'
+  end
+
+  target 'HelloWordUITests' do
+    # Pods for testing
+  end
+
+end
